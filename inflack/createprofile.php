621be370682c51ae5de96403f172afa9031c01f5
rakinplaban <rakinshahriar54@gmail.com>
2021-11-19 21:05:53 +0600

    read unsuccessful.

--- inflack/createprofile.php
+++ inflack/createprofile.php
@@ -9,15 +9,11 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-danger">
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link text-white" href="index.php">Home</a>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="index.php">Home</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link text-white" href="createprofile.php">Create profile.</a>
-              </li>
-              
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
               </li>
             </ul>
           </div>
--- inflack/index.php
+++ inflack/index.php
@@ -9,20 +9,50 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-danger">
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
+              <li class="nav-item">
                 <a class="nav-link text-white" href="index.php">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-white" href="createprofile.php">Create profile.</a>
-              </li>
-              
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
+                <a class="nav-link text-light" href="createprofile.php">Create profile</a>
               </li>
             </ul>
           </div>
         </nav>
-        <h1>Hi</h1>
-        
+        <?php
+          $servername = "localhost";
+          $username = "root";
+          $password = "";
+          $dbname = "inflack";
+    
+          $conn = mysqli_connect($servername, $username, $password, $dbname);
+    
+          if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+          }
+    
+          $sql = "SELECT * from inflackusers";
+          $result = mysqli_query($conn, $sql);
+          while($rows = mysqli_fetch_assoc($result)){
+        ?>
+            <table class = "table">
+              <tr>
+                <th>Serial</th>
+                <th>Name</th>
+                <th>Father's Name</th>
+                <th>Profile Picture</th>
+                <th>Action</th>
+              </tr>
+              <tr>
+                <td scope="row"><?php echo $rows['id']; ?></td>
+                <td><?php echo $rows['uname']; ?></td>
+                <td><?php echo $rows['fname']; ?></td>
+                <td><?php echo '<img src="data:image;base64,'.base64_encode($rows['img']).'" alt="image currently not available!" style="width:50px; height:30px" >'; ?></td>
+                <td><a href="delete.php" class="btn btn-danger">Delete</a></td>
+              </tr>
+            </table>
+        <?php
+          }
+        ?>
+       
     </body>
 </html>
